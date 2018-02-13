0c22bad8d5f1a0cb372b87ee4c721e482e6ed761
Maël Gargadennec <mael.gargadennec@gmail.com>
2018-02-13 17:27:01 +0100

    Package change (again !)

---
+++ sample-complete/src/main/java/com/blossomproject/sample/MainController.java
@@ -0,0 +1,69 @@
+package com.blossomproject.sample;
+
+import com.blossomproject.core.group.GroupDTO;
+import com.blossomproject.core.group.GroupService;
+import com.blossomproject.core.role.RoleDTO;
+import com.blossomproject.core.role.RoleService;
+import com.blossomproject.core.user.UserDTO;
+import com.blossomproject.core.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by maelg on 05/05/2017.
+ */
+@Controller
+@RequestMapping
+public class MainController {
+
+  @Autowired
+  private Random random;
+  @Autowired
+  private UserService userService;
+  @Autowired
+  private RoleService roleService;
+  @Autowired
+  private GroupService groupService;
+
+  @GetMapping
+  public String home(Model model, HttpServletResponse response) throws InterruptedException {
+    long sleep = (long) (random.nextGaussian() * 1000f);
+    if (sleep > 0) {
+      Thread.sleep(sleep);
+    }
+
+    if (random.nextDouble() > 0.8d) {
+      response.setStatus(HttpStatus.values()[random.nextInt(HttpStatus.values().length)].value());
+    }
+
+    model.addAttribute("users", userService.getAll());
+    return "home";
+  }
+
+  @GetMapping("/users")
+  @ResponseBody
+  public List<UserDTO> users() {
+    return userService.getAll();
+  }
+
+  @GetMapping("/roles")
+  @ResponseBody
+  public List<RoleDTO> roles() {
+    throw new RuntimeException("mael test");
+  }
+
+  @GetMapping("/groups")
+  @ResponseBody
+  public List<GroupDTO> groups() {
+    return groupService.getAll();
+  }
+}
---
+++ sample-war-packaging/src/main/java/com/blossomproject/sample/Application.java
@@ -0,0 +1,23 @@
+
+package com.blossomproject.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application
+  extends SpringBootServletInitializer {
+
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
+  }
+
+  public final static void main(String... args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
